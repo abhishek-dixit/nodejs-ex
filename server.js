7d2dca4d40f6d8d710e1348efa6fc0e8184cc669
abhishek.dixit <abhi141981>
2018-07-19 22:11:10 +0530

    A

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var server_port =process.env.PORT || 8080;
+
 let express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
@@ -32,4 +34,9 @@ app.post('/forecast', requestVerifier, function(req, res) {
       }
     });
   }
+});
+
+app.listen(server_port, function(){
+  console.log( "Listening on server_port " + server_port  );
+  //JaiHanuman//Jai Sree Ram
 });
