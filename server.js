bc45c4ffbd18ae3996972ac5cfb6d70453ba2da5
abhishek.dixit <abhi141981>
2018-07-19 13:57:37 +0530

    A

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-// var port = process.env.PORT || 8080;
-
+var server_port =process.env.PORT || 8080;
 
 let express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
-
-// app.use(bodyParser.json());
-// app.post('/forecast', function(req, res) {
-//     // We'll fill this out later!
-//     res.json({ hello: 'world' });
-// });
-
-
-var server_port =process.env.PORT || 8080;
-
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 //app.use(bodyParser.json());
 app.get('/forecast', function(req, res) {
     // We'll fill this out later!
     res.json({ hello: 'world' });
 });
-
-// app.listen(server_port, server_ip_address, function () {
-    
-// });
 
 app.listen(server_port, function(){
     console.log( "Listening on server_port " + server_port  );
